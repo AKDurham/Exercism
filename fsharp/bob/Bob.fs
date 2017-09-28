785b4a25bd30e043569ee88f092f2fafc866fd38
Durham <Adam_Durham@AMER.DELL.COM>
2017-09-28 13:07:20 -0500

    Bob solution!

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,3 +1,20 @@
 ﻿module Bob
 
-let hey (input: string): string = failwith "You need to implement this function."
+let isUpper (input: string): bool =
+    input.ToUpper() = input && input.ToLower() <> input
+
+let isQuestion (input: string): bool =
+    input.EndsWith("?")
+
+let isExclamation (input: string): bool =
+    input.EndsWith("!")
+
+let isYelling (input: string): bool =
+    isUpper(input) || isExclamation(input)
+
+let hey (input: string): string = 
+    match input with
+    | input when (input.Trim() = "") -> "Fine. Be that way!"
+    | input when (isYelling(input)) -> "Whoa, chill out!"
+    | input when (isQuestion(input)) -> "Sure."
+    | _ -> "Whatever."
--- fsharp/bob/BobTest.fs
+++ fsharp/bob/BobTest.fs
@@ -8,58 +8,58 @@ open Bob
 let ``Stating something`` () =
     hey "Tom-ay-to, tom-aaaah-to." |> should equal "Whatever."
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Shouting`` () =
     hey "WATCH OUT!" |> should equal "Whoa, chill out!"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Asking a question`` () =
     hey "Does this cryogenic chamber make me look fat?" |> should equal "Sure."
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Asking a numeric question`` () =
     hey "You are, what, like 15?" |> should equal "Sure."
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Forceful questions`` () =
     hey "WHAT THE HELL WERE YOU THINKING?" |> should equal "Whoa, chill out!"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Shouting numbers`` () =
     hey "1, 2, 3 GO!" |> should equal "Whoa, chill out!"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Only numbers`` () =
     hey "1, 2, 3" |> should equal "Whatever."
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Question only with numbers`` () =
     hey "4?" |> should equal "Sure."
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Shouting with special characters`` () =
     hey "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!" |> should equal "Whoa, chill out!"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Shouting with no exlamation mark`` () =
     hey "I HATE YOU" |> should equal "Whoa, chill out!"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Statement containing question mark`` () =
     hey "Ending with ? means a question." |> should equal "Whatever."
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Prattling on`` () =
     hey "Wait! Hang on. Are you going to be OK?" |> should equal "Sure."
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Silence`` () =
     hey "" |> should equal "Fine. Be that way!"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Prolonged silence`` () =
     hey "    " |> should equal "Fine. Be that way!"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Multiple line question`` () =
     hey "Does this cryogenic chamber make me look fat?\nno" |> should equal "Whatever."
