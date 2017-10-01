ad1315db8b2a81a2d48dc3ddf33ca6cabb5d3208
AKDurham <adam.k.durham@gmail.com>
2017-10-01 10:38:11 -0500

    Added Leap
    Merged with home desktop version.
    Added gitignore to force executables and other binaries to not be checked in.
    Added F# Leap to repo

---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,5 @@
+﻿module LeapYear
+
+let isCentury (year: int): bool = year % 100 = 0;
+let isLeapCentury (year: int): bool = year % 400 = 0;
+let isLeapYear (year: int): bool = isLeapCentury(year) || not (isCentury(year)) && year % 4 = 0;
