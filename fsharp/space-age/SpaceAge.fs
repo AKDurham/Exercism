8841e147e46018906003571fdccc6cea73582d6f
Durham <Adam_Durham@AMER.DELL.COM>
2017-10-04 21:28:11 -0500

    Space Age Solution

--- fsharp/space-age/SpaceAge.fs
+++ fsharp/space-age/SpaceAge.fs
@@ -1,5 +1,19 @@
 ﻿module SpaceAge
 
-// TODO: define the Planet type
+type Planet = Earth | Mercury | Venus | Mars | Jupiter | Saturn | Uranus | Neptune
 
-let spaceAge (planet: Planet) (seconds: decimal): decimal = failwith "You need to implement this function."
+let earthOrbitalPeriod = 31557600m;
+
+let spaceAge (planet: Planet) (seconds: decimal): decimal = 
+    let planetaryYear = match planet with
+        | Earth -> earthOrbitalPeriod
+        | Mercury -> earthOrbitalPeriod * 0.2408467m
+        | Venus -> earthOrbitalPeriod * 0.61519726m
+        | Mars -> earthOrbitalPeriod * 1.8808158m
+        | Jupiter -> earthOrbitalPeriod * 11.862615m
+        | Saturn -> earthOrbitalPeriod * 29.447498m
+        | Uranus -> earthOrbitalPeriod * 84.016846m
+        | Neptune -> earthOrbitalPeriod * 164.79132m
+
+    let precisePlanetaryAge = seconds / planetaryYear
+    System.Decimal.Round(precisePlanetaryAge, 2)
